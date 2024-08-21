f18393f9ddae159efafae20623388d32a11a1665
muhammed ajmal <muhammedajmalcc6424094@gmail.com>
2024-08-21 12:21:19 +0530

    clear some bugs color btn

--- src/components/Button/ButtonStyle.js
+++ src/components/Button/ButtonStyle.js
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 
-
- export const Div = styled.div`
+export const Div = styled.div`
   background-color: ${(props) => props.bgColor || 'gray'};
-  color:transparent;
+  color: transparent;
   /* padding: 20px; */
-  width: 30px;
-  height: 30px;
+  width: 20px;
+  height: 20px;
   margin: 10px 0;
   border-radius: 50%;
+  transition: transform 0.2s ease-in-out;
+  border: 3px solid rgba(128, 128, 128, ${props => props.borderOpacity || 0.8});
+  &:hover {
+    transform: scale(1.3);
+    cursor: pointer;
+  }
 `;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,13 +3,11 @@ import Navbar from "../../components/navbar/Navbar/Navbar";
 import Card from "../../components/Card/Card";
 import { Container } from "../Home/Homestyle";
 import Cycle from '../../components/Cycle/Cycle'
-import Caroseal from '../../components/Caroseal/Carosesal'
 const Home = () => {
   return (
     <div>
       <Container>
         <Navbar />
-        {/* <Caroseal/> */}
       <Card />
 <Cycle/>
       </Container>
